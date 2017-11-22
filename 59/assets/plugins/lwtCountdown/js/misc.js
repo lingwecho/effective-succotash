9123a78f1d15565728ef7c9a3a21498b71e76841
icegeeker <1602126045@qq.com>
2017-11-22 10:14:36 +0800

    1
    1

---
+++ 59/assets/plugins/lwtCountdown/js/misc.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+
+  "use strict";
+
+      $('#countdown').countDown({
+        targetOffset: {
+            'day':      0,
+            'month':    0,
+            'year':     0,
+            'hour':     130,
+            'min':      0,
+            'sec':      3
+        }
+      });
+    })
